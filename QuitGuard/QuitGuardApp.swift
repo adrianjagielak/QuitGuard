67c8ff175b99e3452d6f1abc4a77c70b635bdcf5
Adrian Jagielak <adrianjagielak@gmail.com>
2024-09-02 13:41:03 +0200

    Initial Commit

---
+++ QuitGuard/QuitGuardApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuitGuardApp.swift
+//  QuitGuard
+//
+//  Created by Adrian Jagielak on 02/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct QuitGuardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
